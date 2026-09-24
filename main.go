e0ae0cadb38897d7aa86cae1d7a51c64367a4314
tan <tancodes@proton.me>
2026-09-24 16:58:28 -0600

    Format the entrypoint for release validation

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"github.com/crowquillx/silo-anime-themes/internal/app"
 	"github.com/crowquillx/silo-theme-songs/pkg/pluginapp"
 )
+
 //go:embed manifest.json
 var manifestJSON []byte
 var version string
-func main(){pluginapp.Main(manifestJSON,version,"animethemes",true,app.New)}
+
+func main() { pluginapp.Main(manifestJSON, version, "animethemes", true, app.New) }
